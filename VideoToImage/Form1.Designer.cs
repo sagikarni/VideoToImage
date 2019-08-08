c68065864171f595bbe9b1846ec766e733b4c76b
Sagi Karni <sagikarni76@gmail.com>
2019-08-08 23:52:47 +0300

    111

--- VideoToImage/Form1.Designer.cs
+++ VideoToImage/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlVideos = new System.Windows.Forms.Panel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer3 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer4 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer5 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer6 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer7 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer8 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer9 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.pnlVideos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlVideos
+            // 
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer7);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer8);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer9);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer4);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer5);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer6);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer3);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer2);
+            this.pnlVideos.Controls.Add(this.axWindowsMediaPlayer1);
+            this.pnlVideos.Location = new System.Drawing.Point(29, 48);
+            this.pnlVideos.Name = "pnlVideos";
+            this.pnlVideos.Size = new System.Drawing.Size(1354, 883);
+            this.pnlVideos.TabIndex = 7;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(14, 3);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(437, 3);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer2.TabIndex = 1;
+            // 
+            // axWindowsMediaPlayer3
+            // 
+            this.axWindowsMediaPlayer3.Enabled = true;
+            this.axWindowsMediaPlayer3.Location = new System.Drawing.Point(860, 3);
+            this.axWindowsMediaPlayer3.Name = "axWindowsMediaPlayer3";
+            this.axWindowsMediaPlayer3.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer3.OcxState")));
+            this.axWindowsMediaPlayer3.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer3.TabIndex = 2;
+            // 
+            // axWindowsMediaPlayer4
+            // 
+            this.axWindowsMediaPlayer4.Enabled = true;
+            this.axWindowsMediaPlayer4.Location = new System.Drawing.Point(860, 288);
+            this.axWindowsMediaPlayer4.Name = "axWindowsMediaPlayer4";
+            this.axWindowsMediaPlayer4.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer4.OcxState")));
+            this.axWindowsMediaPlayer4.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer4.TabIndex = 5;
+            // 
+            // axWindowsMediaPlayer5
+            // 
+            this.axWindowsMediaPlayer5.Enabled = true;
+            this.axWindowsMediaPlayer5.Location = new System.Drawing.Point(437, 288);
+            this.axWindowsMediaPlayer5.Name = "axWindowsMediaPlayer5";
+            this.axWindowsMediaPlayer5.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer5.OcxState")));
+            this.axWindowsMediaPlayer5.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer5.TabIndex = 4;
+            // 
+            // axWindowsMediaPlayer6
+            // 
+            this.axWindowsMediaPlayer6.Enabled = true;
+            this.axWindowsMediaPlayer6.Location = new System.Drawing.Point(14, 288);
+            this.axWindowsMediaPlayer6.Name = "axWindowsMediaPlayer6";
+            this.axWindowsMediaPlayer6.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer6.OcxState")));
+            this.axWindowsMediaPlayer6.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer6.TabIndex = 3;
+            // 
+            // axWindowsMediaPlayer7
+            // 
+            this.axWindowsMediaPlayer7.Enabled = true;
+            this.axWindowsMediaPlayer7.Location = new System.Drawing.Point(860, 573);
+            this.axWindowsMediaPlayer7.Name = "axWindowsMediaPlayer7";
+            this.axWindowsMediaPlayer7.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer7.OcxState")));
+            this.axWindowsMediaPlayer7.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer7.TabIndex = 8;
+            // 
+            // axWindowsMediaPlayer8
+            // 
+            this.axWindowsMediaPlayer8.Enabled = true;
+            this.axWindowsMediaPlayer8.Location = new System.Drawing.Point(437, 573);
+            this.axWindowsMediaPlayer8.Name = "axWindowsMediaPlayer8";
+            this.axWindowsMediaPlayer8.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer8.OcxState")));
+            this.axWindowsMediaPlayer8.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer8.TabIndex = 7;
+            // 
+            // axWindowsMediaPlayer9
+            // 
+            this.axWindowsMediaPlayer9.Enabled = true;
+            this.axWindowsMediaPlayer9.Location = new System.Drawing.Point(14, 573);
+            this.axWindowsMediaPlayer9.Name = "axWindowsMediaPlayer9";
+            this.axWindowsMediaPlayer9.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer9.OcxState")));
+            this.axWindowsMediaPlayer9.Size = new System.Drawing.Size(417, 279);
+            this.axWindowsMediaPlayer9.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(831, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(408, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(416, 20);
+            this.txtPath.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1463, 969);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.pnlVideos);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.pnlVideos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlVideos;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer7;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer8;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer9;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer4;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer5;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer6;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer3;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPath;
     }
 }
 
--- VideoToImage/Form1.cs
+++ VideoToImage/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AxWMPLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,51 @@ namespace VideoToImage
         public Form1()
         {
             InitializeComponent();
+
+            //using (var engine = new Engine(@"C:\Users\sagik\source\repos\VideoSnapShot\VideoSnapShot\bin\Debug\ffmpeg.exe"))
+            //{
+            //    var mp4 = new MediaFile { Filename = @"C:\Users\sagik\Downloads\GettyImages-527762845.mov" };
+            //    var options = new ConversionOptions { Seek = TimeSpan.FromSeconds(10) };
+            //    var outputFile = new MediaFile { Filename = string.Format("{0}\\image-{1}.jpeg", @"C:\Users\sagik\Downloads\opera autoupdate", 10) };
+
+            //    engine.GetMetadata(mp4);
+            //    engine.GetThumbnail(mp4, outputFile, options);
+            //}
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            var files = Directory.GetFiles(txtPath.Text, "*.mp4");
+
+            foreach (string file in files)
+            {
+                var video = (AxWindowsMediaPlayer)pnlVideos.Controls[8 - index];
+                video.URL = (file);
+                video.PlayStateChange += pauseOnFirstFrame;
+
+                video.Ctlcontrols.play();
+                index++;
+
+            }
+            while (index < 9)
+            {
+                pnlVideos.Controls.RemoveAt(0);
+                index++;
+            }
+            pnlVideos.Visible = true;
+        }
+
+
+        void pauseOnFirstFrame(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 3) // playing
+            {
+                (sender as AxWindowsMediaPlayer).Ctlcontrols.pause();
+                (sender as AxWindowsMediaPlayer).Ctlcontrols.currentPosition = 0;
+                (sender as AxWindowsMediaPlayer).PlayStateChange -= pauseOnFirstFrame;
+            };
+        }
+
     }
 }
